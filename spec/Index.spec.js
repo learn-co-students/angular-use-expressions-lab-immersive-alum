e2b86a191faa81491de8afbcc606171af3bc9d15
ipc103 <iancandysemail@gmail.com>
2016-02-29 13:56:59 -0500

    update test descriptions

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,11 +3,11 @@ var path = require('path');
 describe('Expressions Test', function() {
 	browser.get('http://localhost:8080');
 
-	it('should say no emails', function() {
+	it('should say "You have no emails" if emails is empty', function() {
 		expect(element(by.css('h6')).getInnerHtml()).toEqual('You have no emails');
 	});
 
-	it('should say the number of emails', function() {
+	it('should say the number of emails if emails is not empty', function() {
 		element(by.css('button')).click();
 
 		expect(element(by.css('h6')).getInnerHtml()).toEqual('You have 1 emails');
