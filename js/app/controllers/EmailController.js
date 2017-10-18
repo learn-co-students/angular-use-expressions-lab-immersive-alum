654c817604caf4d07fda1e672615c4f041cc9c8b
AllA <allagorik2901@gmail.com>
2017-10-18 18:21:22 -0400

    Done.

--- js/app/controllers/EmailController.js
+++ js/app/controllers/EmailController.js
@@ -1,14 +1,14 @@
 function EmailController($filter) {
-    this.emails = [];
+    this.emails = []
 
     this.addEmail = function () {
         this.emails.push({
             name: 'Bill Gates',
             subject: 'Want to buy a Surface?'
         })
-    };
+    }
 }
 
 angular
     .module('app')
-    .controller('EmailController', EmailController);
+    .controller('EmailController', EmailController)
